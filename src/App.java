e42a957fd45c7c703dd52caa3d7db5f829d27a3a
xasan <xasanboyevbobur1402@gmail.com>
2020-10-19 22:42:21 +0500

    App

---
+++ src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args){
+
+        //1 st question
+// Multiples.start();
+
+        //2 nd question
+//PerfectNumber.start(10000);
+
+        //3 rd question
+// ReverseDigit.start();
+
+        // 4 th question
+//Duplicate.start();
+
+        //5 th question
+//AirlineReservationSystem.start();
+
+        //6 th question
+//SieveOFEratosthenes.start();
+
+
+    }
+}
